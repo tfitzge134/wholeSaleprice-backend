a920d766d1509b6a9836b3ada90b9691b49ecd27
TERESA Hidalgo De Fitzgerald <tfitzge134@gmail.com>
2020-03-11 17:59:07 -0400

    setup env config

--- db/models/connection.js
+++ db/models/connection.js
@@ -1,14 +1,10 @@
 const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-dotenv.config();
 
-mongoose
-  .connect(process.env.DATABASE_URL, { useNewUrlParser: true })
-  .then(conn => {
-    console.log(
-      `database successfully connected on ${conn.connections[0].name}`
-    );
-  })
-  .catch(err => console.log('error connecting to database', err));
+
+mongoose.connect("mongodb://localhost/miso", { useNewUrlParser: true })
+.then(conn => {
+    console.log(`database successfully connected on ${conn.connections[0].name}`)
+})
+.catch(err => console.log("error connecting to database", err));
 
 module.exports = mongoose;
